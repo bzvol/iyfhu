1204c7c7fbfef09f809ef957e5cf8875376dae21
Benjamin Zvolenszki <zvolenszki.benjamin@gmail.com>
2023-10-29 21:06:11 +0100

    carousel animation

--- src/pages/index/Carousel.tsx
+++ src/pages/index/Carousel.tsx
@@ -2,7 +2,7 @@
 // noinspection HtmlRequiredAltAttribute
 
 import './styles/Carousel.scss';
-import {useEffect, useState} from "react";
+import {RefObject, useEffect, useRef, useState} from "react";
 
 interface CarouselProps {
     images: Array<{
@@ -16,18 +16,27 @@ interface CarouselProps {
 
 export default function Carousel({images, timePerSlide = 5., transitionTime = 2.}: CarouselProps) {
     const [currentSlide, setCurrentSlide] = useState(0);
+    const currentImg = useRef<HTMLImageElement>(null);
+    const nextImg = useRef<HTMLImageElement>(null);
 
     useEffect(() => {
-        const nextSlide = () => setCurrentSlide(prev => (prev + 1) % images.length);
-        
-        const interval = setInterval(nextSlide, timePerSlide * 1000);
+        [currentImg, nextImg].forEach(ref => ref.current?.style.setProperty('--transition-time', `${transitionTime}s`));
+
+        const toggleRefsActive = () => [currentImg, nextImg].forEach(ref => ref.current?.classList.toggle('active'));
+        const interval = setInterval(() => {
+            toggleRefsActive();
+            setTimeout(() => {
+                toggleRefsActive();
+                setCurrentSlide(prev => (prev + 1) % images.length);
+            }, 1000 * transitionTime);
+        }, 1000 * (timePerSlide + transitionTime));
         return () => clearInterval(interval);
-    }, [images, timePerSlide]);
+    }, [images, timePerSlide, transitionTime]);
 
     return (
         <section className="Carousel">
-            <img className="Carousel__img current" {...(images[currentSlide])}/>
-            <img className="Carousel__img next" {...(images[(currentSlide + 1) % images.length])}/>
+            <img ref={currentImg} {...(images[currentSlide])}/>
+            <img ref={nextImg} {...(images[(currentSlide + 1) % images.length])}/>
         </section>
     );
 }
